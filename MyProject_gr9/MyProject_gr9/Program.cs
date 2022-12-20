54ecb51f79aa9408f242e90ef7fae50263596e00
grishagr9 <56480294+grishagr9@users.noreply.github.com>
2022-12-20 21:33:26 +0300

    Update Program.cs

--- MyProject_gr9/MyProject_gr9/Program.cs
+++ MyProject_gr9/MyProject_gr9/Program.cs
@@ -14,7 +14,7 @@ namespace MyProject
         }
         static void Main(string[] args) {
             int a = 5;
-            int sdvsdv
+            int axaxaxaa
                 //int.Parse(Console.ReadLine());
             Console.WriteLine(Factorial(a));
         }
